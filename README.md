35d432e7e98770a13ef58cd350640dde93a87beb
Anjali Kantharuban <anjaliruban@berkeley.edu>
2020-10-27 22:48:53 -0700

    Screw the world

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# PSEG897
+# Planetary Dataset
+Anjali Kantharuban, Alok Elashoff, Rushil Kapadia, Nikhil Mandava, and Jinyoung Bae
+
+## Description
+
+
+## Generating Data
+
+This dataset contains a list of planetary data in the following format:
+```
+{
+  'time': "YYYY-MM-DD",
+  'image': "image_folder/YYYY-MM-DD.png",
+  'planet_data': {
+    'mercury': {
+      'az': azimuth,
+      'alt': altitude,
+      'ra': right_ascension,
+      'dec': declination,
+      'dist': distance
+    }
+    ...
+  }
+}
+```
+
+There are seven available planets - Mercury, Venus, Mars, Jupiter, Saturn, Neptune, and Uranus. When generating this dataset with default arguments, only the first five are included because before the advent of modern instruments, Neptune and Uranus when not visible.
+
+When generating the dataset with custom parameters there are a series of flags that can be used:
+```
+-h, --help            show this help message and exit
+--T T                 The time from which you want to generate data in the
+                        format YYYY-MM-DD
+--dt DT               The timestep between data points in days.
+--timesteps TIMESTEPS
+                      The number of data points to generate.
+--planets PLANETS [PLANETS ...]
+                      Which planets out of (mercury, venus, mars, jupiter,
+                      saturn, neptune, uranus) to include in the dataset.
+--long LONG           The longitude of the location on earth the data is
+                      observed from in degrees.
+--lat LAT             The longitude of the location on earth the data is
+                      observed from in degrees.
+--alt_error ALT_ERROR
+                      The maximum error in the measurement of altitude in
+                      degrees.
+--az_error AZ_ERROR   The maximum error in the measurement of azmith in
+                      degrees.
+--ra_error RA_ERROR   The maximum error in the measurement of right
+                      ascension in hours.
+--dec_error DEC_ERROR
+                      The maximum error in the measurement of declination in
+                      degrees.
+--dist_error DIST_ERROR
+                      The maximum error in the measurement of distance in
+                      kilometers.
+--data_file DATA_FILE
+                      The data file to which the data json is stored.
+--image               Generate images for each data point.
+--image_folder IMAGE_FOLDER
+                      The folder to which images are stored
+```
--- generate_data.py
+++ generate_data.py
@@ -24,11 +24,15 @@ if __name__ == '__main__':
     parser.add_argument('--long', help='The longitude of the location on earth the data is observed from in degrees.', default=20.5937, type=float)
     parser.add_argument('--lat', help='The longitude of the location on earth the data is observed from in degrees.', default=78.9629, type=float)
 
+    parser.add_argument('--tools', help='The list of tools used to calculate error. You can add more tools in tools.json.', default=[], nargs='+')
+    parser.add_argument('--tools_file', help='The json files that contain data about each tool,', default="tools.json")
     parser.add_argument('--alt_error', help='The maximum error in the measurement of altitude in degrees.', default=0.0, type=float)
     parser.add_argument('--az_error', help='The maximum error in the measurement of azmith in degrees.', default=0.0, type=float)
     parser.add_argument('--ra_error', help='The maximum error in the measurement of right ascension in hours.', default=0.0, type=float)
     parser.add_argument('--dec_error', help='The maximum error in the measurement of declination in degrees.', default=0.0, type=float)
     parser.add_argument('--dist_error', help='The maximum error in the measurement of distance in kilometers.', default=0.0, type=float)
+
+    parser.add_argument('--data_file', help="The data file to which the data json is stored.", default="planet_data.json")
 
     parser.add_argument('--image', help='Generate images for each data point.', action='store_true')
     parser.add_argument('--image_folder', help='The folder to which images are stored', default='image_data')
@@ -45,15 +49,47 @@ if __name__ == '__main__':
 
     ### Initalize Planet Information ###
     earth = raw_data['earth']
+    possible_planets = ['mercury', 'mars', 'venus', 'jupiter', 'saturn', 'neptune', 'uranus']
     longitude = str(args.long) + ' N'
     latitude = str(args.lat) + ' E'
     position = earth + Topos('20.5937 N', '78.9629 E')
-    planets = ['mercury', 'mars', 'venus', 'jupiter', 'saturn'] if not args.planets else args.planets
+    planets = ['mercury', 'mars', 'venus', 'jupiter', 'saturn'] if not args.planets else [planet.lower() for planet in args.planets]
+    for planet in planets:
+        if planet not in possible_planets:
+            raise ValueError('One or more planets you have passed in are not valid arguments.')
     ts = load.timescale()
 
     ### Set Up Time Data ###
     t = datetime.datetime.strptime(args.T, "%Y-%m-%d")
     t_delta = datetime.timedelta(days=args.dt)
+
+    ### Define Error For Each Measurement Based Off Of Inputted Tools ###
+    alt_error = []
+    az_error = []
+    ra_error = []
+    dec_error = []
+    dist_error = []
+    with open(args.tools_file, 'rb') as file:
+        possible_tools = json.load(file)
+    for tool in args.tools:
+        if tool.lower() not in possible_tools.keys():
+            raise ValueError('One or more tools you have passed in are not valid arguments.')
+        else:
+            if 'alt' in possible_tools[tool].keys():
+                alt_error.append(possible_tools[tool]['alt'])
+            if 'az' in possible_tools[tool].keys():
+                az_error.append(possible_tools[tool]['az'])
+            if 'ra' in possible_tools[tool].keys():
+                ra_error.append(possible_tools[tool]['ra'])
+            if 'dec' in possible_tools[tool].keys():
+                dec_error.append(possible_tools[tool]['dec'])
+            if 'dist' in possible_tools[tool].keys():
+                dist_error.append(possible_tools[tool]['dist'])
+    alt_error = min(alt_error) if alt_error else args.alt_error
+    az_error = min(az_error) if az_error else args.az_error
+    ra_error = min(rat_error) if ra_error else args.ra_error
+    dec_error = min(dec_error) if dec_error else args.dec_error
+    dist_error = min(dist_error) if dist_error else args.dist_error
 
     ### Create Data Array ###
     processed_data = []
@@ -74,11 +110,12 @@ if __name__ == '__main__':
             alt, az, _ = astrometric.apparent().altaz()
             ra, dec, dist = astrometric.apparent().radec(epoch = "date")
 
-            alt = alt.degrees + np.random.normal(0, args.alt_error / 3)
-            az = az.degrees + np.random.normal(0, args.az_error / 3)
-            ra = ra.hours + np.random.normal(0, args.ra_error / 3)
-            dec = dec.degrees + np.random.normal(0, args.dec_error / 3)
-            dist = dist.km + np.random.normal(0, args.dist_error / 3)
+            ### Adding Noise ###
+            alt = alt.degrees + np.random.normal(0, alt_error / 3)
+            az = az.degrees + np.random.normal(0, az_error / 3)
+            ra = ra.hours + np.random.normal(0, ra_error / 3)
+            dec = dec.degrees + np.random.normal(0, dec_error / 3)
+            dist = dist.km + np.random.normal(0, dist_error / 3)
 
             ### Save Planet Data with Error ###
             current_dict['planet_data'][p] = {
@@ -100,5 +137,5 @@ if __name__ == '__main__':
         t += t_delta
 
     ### Dump Data Array ###
-    with open("data.json", 'w') as file:
-        json.dump(processed_data, file)
+    with open(args.data_file, 'w') as file:
+        json.dump(processed_data, file, indent=4)
